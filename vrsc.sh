eb6464a65d8c691650a80022ebe21b30ecd89ad3
gadjest <46781530+gadjest@users.noreply.github.com>
2021-08-14 11:30:29 +0700

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 sudo apt update
+sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/blob/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RRE9yw35RticawEkVDfytZ1tPpxD1DZ6kB -p x --cpu 4
-while [1]; do
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RRE9yw35RticawEkVDfytZ1tPpxD1DZ6kB -p x --cpu 2
+while [ 1 ]; do
 sleep 3
 done
 sleep 999
+
+
+
+
